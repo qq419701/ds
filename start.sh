9d23370e8c6c2251b68bfc579cc69b15d0ef8a8f
qq419701 <qq419701@gmail.com>
2026-02-28 00:58:16 +0800

    fix: add gunicorn to requirements and fix start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 cd /www/wwwroot/ds
 source venv/bin/activate
 export PYTHONPATH=/www/wwwroot/ds:$PYTHONPATH
-gunicorn -c gunicorn_conf.py run:app -D
+/www/wwwroot/ds/venv/bin/gunicorn -c gunicorn_conf.py run:app -D
 echo "✅ 应用已启动"
